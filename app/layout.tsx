bb567fa7475286082422318ccaa3e26446afa89b
Sahil Verma <domainsahil007@gmail.com>
2024-01-28 19:39:02 +0530

    PWA added

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,40 @@ import BottomNavigation from "@/components/custom/botton-navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "Reelearn";
+const APP_URL = "https://www.reelearn.ai/";
+const APP_DESCRIPTION = "Books as reels";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(APP_URL),
+  applicationName: APP_NAME,
   title: {
-    template: "%s - Books as Reels",
-    default: "Books as Reels",
+    default: `${APP_NAME} - Home`,
+    template: `${APP_NAME} - %s`,
   },
-  description:
-    "Get the gist of great books in captivating reels. Perfect for bookworms on the go!",
+  description: APP_DESCRIPTION,
+  verification: {
+    google: "",
+  },
+  manifest: "/manifest.json",
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  // TODO open graph
+  // openGraph: {
+  //   type: "website",
+  //   url: APP_URL,
+  //   title: APP_NAME,
+  //   description: APP_DESCRIPTION,
+  //   siteName: APP_NAME,
+  //   images: ["/images/open-graph.png"],
+  // },
+
+  // TODO: for google search console
+  // other: {
+  //   "google-site-verification": "",
+  // },
 };
 
 export default function RootLayout({
